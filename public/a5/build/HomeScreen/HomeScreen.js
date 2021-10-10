c2d5beffe1e2201f3ac1d1ab4696e5d0d844f6e2
Yangjiawen Xu <xuxx1262@umn.edu>
2021-10-10 17:36:54 +0800

    add margin

--- public/a5/build/HomeScreen/HomeScreen.js
+++ public/a5/build/HomeScreen/HomeScreen.js
@@ -9,7 +9,7 @@ import PostList from "../PostList";
             <div class="col-2 col-md-2 col-lg-1 col-xl-2">
                 ${NavigationSidebar('home')}
             </div>
-            <div class="col-10 col-lg-8 col-xl-7 col-xxl-6">
+            <div class="mb-2 col-10 col-lg-8 col-xl-7 col-xxl-6">
                 ${PostList()}
             </div>
             <div class="d-none d-lg-block col-lg-3 col-xl-3 col-xxl-4">
