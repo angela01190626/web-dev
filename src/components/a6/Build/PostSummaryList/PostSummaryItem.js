f4fff7f329e9bb15882d8b5a39eb5b21328a4863
Yangjiawen Xu <xuxx1262@umn.edu>
2021-10-20 11:53:21 +0800

    broken images fixed

--- src/components/a6/Build/PostSummaryList/PostSummaryItem.js
+++ src/components/a6/Build/PostSummaryList/PostSummaryItem.js
@@ -6,7 +6,7 @@ const PostSummaryItem = (
         userName: 'ReactJS',
         time: '2h',
         title: 'React.js is a component based front end library that makes it very easy to build Single Page Applications or SPAs',
-        image: '../../../images/react-blue.png'}
+        image: '/images/react-blue.png'}
         }) => {
     return(
         <div className="row pt-2">
