42610f425290759e65bb1afadf746e38a683df7e
Yangjiawen Xu <xuxx1262@umn.edu>
2021-10-31 22:10:37 +0800

    fix

--- src/App.js
+++ src/App.js
@@ -26,18 +26,18 @@ function App() {
                   <Route path="/a6/hello" exact={true}>
                       <HelloWorld/>
                   </Route>
-                  {/*<Route path={["/", "/a6", "/a6/practice"]} exact={true}>*/}
-                  {/*    <Practice/>*/}
-                  {/*</Route>*/}
+                  <Route path={["/", "/a6", "/a6/practice"]} exact={true}>
+                      <Practice/>
+                  </Route>
                   <Route path="/a6/build" exact={true}>
                       <Build/>
                   </Route>
                   <Route path="/a6/twitter/explore" component={Build}/>
                   <Route path="/a6/twitter/home" component={HomeScreen}/>
 
-                  <Route path={["/", "/a7", "/a7/practice"]} exact={true}>
-                      <Practice/>
-                  </Route>
+                  {/*<Route path={["/", "/a7", "/a7/practice"]} exact={true}>*/}
+                  {/*    <Practice/>*/}
+                  {/*</Route>*/}
                   {/*<Route path="/a7/twitter/home" component={HomeScreen}/>*/}
                   {/*<Route path="/a7/twitter/home" component={ExploreScreen}/>*/}
 
