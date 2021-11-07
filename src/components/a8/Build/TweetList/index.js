c6af3d7e5fa972df8ac8cc5b351c532c96776edf
Yangjiawen Xu <xuxx1262@umn.edu>
2021-11-07 18:48:01 +0800

    fix deploy

--- src/components/a8/Build/TweetList/index.js
+++ src/components/a8/Build/TweetList/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {useDispatch,useSelector} from "react-redux";
+import {useSelector} from "react-redux";
 import TweetListItem from "./TweetListItem";
 
 const selectAllTweets = (state) => state.tweets.tweets;
@@ -7,7 +7,7 @@ const selectAllTweets = (state) => state.tweets.tweets;
 const TweetList = () => {
 
     const tweets = useSelector(selectAllTweets);
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     // useEffect(() => fetchAllTweets(dispatch), []);
 
 
