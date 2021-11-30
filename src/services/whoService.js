574fa961d5674bc63c8ead4d038600003686a392
Yangjiawen Xu <xuxx1262@umn.edu>
2021-11-30 21:09:11 +0800

    fix

--- src/services/whoService.js
+++ src/services/whoService.js
@@ -1,5 +1,5 @@
-// const WHO_API = 'http://localhost:4000/rest/who';
-const WHO_API = 'https://safe-thicket-27885.herokuapp.com/rest/who';
+const WHO_API = 'http://localhost:4000/rest/who';
+// const WHO_API = 'https://safe-thicket-27885.herokuapp.com/rest/who';
 
 
 export const fetchAllWho = () =>
